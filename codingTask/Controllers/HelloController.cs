d507955414493f4d3b3ddd0054f8942ba708e356
jaypitti <jaypitti@WIN-62GDBT41VHF>
2017-11-29 17:38:26 -0600

    Hello World API

---
+++ codingTask/Controllers/HelloController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Http;
+
+namespace codingTask.Controllers
+{
+    public class HelloController : ApiController
+    {
+        // GET: api/Hello
+        public IHttpActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+
+        //// GET: api/Hello/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Hello
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT: api/Hello/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE: api/Hello/5
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
